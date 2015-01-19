1bbc1effe722c50bdc7173a0cfbd02069de6f128
hhannu <hth@bowmore..example.com>
2015-01-19 13:33:18 +0200

    Added initial login page. Use stylus + jquery mobile.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lenkit' });
+    // user is not logged in
+    if(typeof req.session.username == 'undefined')
+        res.render('login', { title: 'Lenkit' });        
+    else
+        res.render('index', { title: 'Lenkit' });
+});
+
+// logout
+router.get('/logout', function(req, res) {
+    req.session.destroy(function(err){});    
+    res.redirect('/');
+});
+
+router.get('/login', function(req, res) {   
+    res.redirect('/');  
+});
+
+router.post('/login', function(req, res) {
+    username = req.body.username || 'Anonymous';
+    req.session.username = username;
+    res.redirect('/');
 });
 
 module.exports = router;
